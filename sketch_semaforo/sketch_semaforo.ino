74f10660ff6b4cc536d540143a7f50a7826656ea
Davide <davide.moro@barsanti.edu.it>
2019-01-30 13:18:07 +0100

    30/01/2019 - Progetto iniziale

---
+++ sketch_semaforo/sketch_semaforo.ino
@@ -0,0 +1,54 @@
+ 
+int tempo1 = 4000;
+int tempo2 = 2000;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  pinMode(13, OUTPUT); // verde
+  pinMode(12, OUTPUT); // giallo
+  pinMode(11, OUTPUT); // rosso
+  pinMode(2, OUTPUT); // rosso1
+  pinMode(3, OUTPUT); // giallo1
+  pinMode(4,OUTPUT); // verde1
+  
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  digitalWrite(13, HIGH);
+  digitalWrite(2, HIGH);
+  delay(tempo1);
+  digitalWrite(13, LOW);
+  digitalWrite(2, LOW);
+  
+  
+  digitalWrite(12, HIGH);
+  digitalWrite(3, HIGH);
+  delay(tempo2);
+  digitalWrite(12, LOW);
+  digitalWrite(3, LOW);
+  
+  
+  digitalWrite(11, HIGH);
+  digitalWrite(4, HIGH);
+  delay(tempo1);
+  digitalWrite(11, LOW);
+  digitalWrite(4, LOW);
+  
+  
+  digitalWrite(12, HIGH);
+  digitalWrite(3, HIGH);
+  delay(tempo2);
+  digitalWrite(12, LOW);
+  digitalWrite(3, LOW);
+  
+  
+}
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
