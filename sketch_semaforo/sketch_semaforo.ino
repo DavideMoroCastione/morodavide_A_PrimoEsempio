3f876364b0debaa4b30df44b5f5a45444f33f532
Davide <davide.moro@barsanti.edu.it>
2019-02-02 09:49:56 +0100

    02/02/2019 - versione finita Moro e Baccega

--- sketch_semaforo/sketch_semaforo.ino
+++ sketch_semaforo/sketch_semaforo.ino
@@ -1,6 +1,7 @@
  
 int tempo1;
 int tempo2;
+int tempo3;
 
 
 
@@ -9,6 +10,7 @@ void setup() {
 
   tempo1 = 4000;
   tempo2 = 2000;
+  tempo3 = 400;
   
   pinMode(13, OUTPUT); // verde
   pinMode(12, OUTPUT); // giallo
@@ -24,34 +26,40 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   
-  digitalWrite(13, HIGH);
-  digitalWrite(2, HIGH);
-  delay(tempo1);
-  digitalWrite(13, LOW);
-  digitalWrite(12, HIGH);
-  digitalWrite(2, HIGH);
-  delay(tempo2);
-  digitalWrite(3, HIGH);
-  delay(tempo2);
-  digitalWrite(12, LOW);
-  digitalWrite(2, LOW);
-  digitalWrite(3, LOW);
-  
-  
   digitalWrite(11, HIGH);
   digitalWrite(4, HIGH);
-  delay(tempo1);
+  numlampeggi(4, tempo3);
+  digitalWrite(3, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(4,LOW);
+  delay(tempo2);
+  digitalWrite(12,LOW);
   digitalWrite(11, LOW);
-  digitalWrite(4, LOW);
-  
-  
+  digitalWrite(13, HIGH);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, LOW);
+  numlampeggi(13, tempo3);
+ 
+  delay(tempo1);
   digitalWrite(12, HIGH);
   digitalWrite(3, HIGH);
   delay(tempo2);
+  digitalWrite(13, LOW);
+  digitalWrite(2, LOW);
   digitalWrite(12, LOW);
   digitalWrite(3, LOW);
   
-  
+}
+
+void numlampeggi( int b, int c)
+{
+  for( int a = 0; a <= 3; a++)
+  {
+    digitalWrite(b, HIGH);
+    delay(tempo3);
+    digitalWrite(b, LOW);
+    delay(tempo3);
+  }
 }
 
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
