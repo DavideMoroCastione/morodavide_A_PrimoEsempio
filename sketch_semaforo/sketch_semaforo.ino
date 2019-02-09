9c032d003146a0d6df7c057fe6c1d4012c34908f
Davide <davide.moro@barsanti.edu.it>
2019-02-09 09:04:53 +0100

    07/02/2019 - versione finale Moro/Baccega

--- sketch_semaforo/sketch_semaforo.ino
+++ sketch_semaforo/sketch_semaforo.ino
@@ -1,16 +1,13 @@
- 
-int tempo1;
-int tempo2;
-int tempo3;
 
+int lampeggi = 0;
+int tempolampeggi = 0;
+int tempoverde = 0;
+int tempogiallo = 0;
 
 
 void setup() {
   // put your setup code here, to run once:
 
-  tempo1 = 4000;
-  tempo2 = 2000;
-  tempo3 = 400;
   
   pinMode(13, OUTPUT); // verde
   pinMode(12, OUTPUT); // giallo
@@ -18,7 +15,10 @@ void setup() {
   pinMode(2, OUTPUT); // rosso1
   pinMode(3, OUTPUT); // giallo1
   pinMode(4,OUTPUT); // verde1
-  
+Lampeggi();
+TempoLampeggi();
+TempoVerde();
+TempoGiallo();
   
 
 }
@@ -28,38 +28,65 @@ void loop() {
   
   digitalWrite(11, HIGH);
   digitalWrite(4, HIGH);
-  numlampeggi(4, tempo3);
-  digitalWrite(3, HIGH);
+
+  numlampeggi(4, tempolampeggi,lampeggi );
   digitalWrite(12, HIGH);
-  digitalWrite(4,LOW);
-  delay(tempo2);
+  digitalWrite(3, HIGH);
+  delay(tempogiallo);
   digitalWrite(12,LOW);
   digitalWrite(11, LOW);
   digitalWrite(13, HIGH);
   digitalWrite(2, HIGH);
   digitalWrite(3, LOW);
-  numlampeggi(13, tempo3);
- 
-  
+  numlampeggi(13, tempolampeggi, lampeggi);
   digitalWrite(12, HIGH);
   digitalWrite(3, HIGH);
-  delay(tempo2);
+  delay(tempogiallo);
   digitalWrite(13, LOW);
   digitalWrite(2, LOW);
   digitalWrite(12, LOW);
-  digitalWrite(3, LOW);
+  digitalWrite(3,LOW);
+  
   
 }
 
-void numlampeggi( int b, int c)
+void numlampeggi( int b, int c, int x)
 {
-  for( int a = 0; a <= 3; a++)
+  for( int a = 0; a < x; a++)
   {
     digitalWrite(b, HIGH);
-    delay(tempo3);
+    delay(tempolampeggi);
     digitalWrite(b, LOW);
-    delay(tempo3);
+    delay(tempolampeggi);
   }
 }
 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+void Lampeggi()
+{
+  Serial.begin(9600),
+  Serial.print( "Quanti lampeggi verdi vuoi?");
+  while(Serial.available() == 0) {};
+  lampeggi = Serial.readString().toInt();
+}
+ void TempoLampeggi()
+{
+  Serial.begin(9600),
+  Serial.print( "Quanto durano i lampeggi del verde?");
+  while(Serial.available() == 0) {};
+  tempolampeggi = Serial.readString().toInt();
+} 
+void TempoVerde()
+{
+  Serial.begin(9600),
+  Serial.print( "Quanto tempo dura il verde?");
+  while(Serial.available() == 0) {};
+  tempoverde = Serial.readString().toInt();
+} 
+void TempoGiallo()
+{
+  Serial.begin(9600),
+  Serial.print( "Quanto tempo dura il giallo?");
+  while(Serial.available() == 0) {};
+  tempogiallo = Serial.readString().toInt();
+
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
